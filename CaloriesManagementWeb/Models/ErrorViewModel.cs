a73db52a1130dc18d020fec16ff5210e851c46f1
Danylo Liulko <kramzigazer@gmail.com>
2025-04-22 03:51:39 +0200

    changed open curly brace style to be on new line

--- CaloriesManagementWeb/Models/ErrorViewModel.cs
+++ CaloriesManagementWeb/Models/ErrorViewModel.cs
@@ -1,5 +1,7 @@
-namespace CaloriesManagementWeb.Models {
-    public class ErrorViewModel {
+namespace CaloriesManagementWeb.Models
+{
+    public class ErrorViewModel
+    {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
--- CaloriesManagementWeb/ViewModel/CalculatorViewModel.cs
+++ CaloriesManagementWeb/ViewModel/CalculatorViewModel.cs
@@ -1,14 +1,16 @@
-﻿namespace CaloriesManagementWeb.ViewModel {
-	public class CalculatorViewModel {
-		public float? Age { get; set; }
-		public string? Gender { get; set; }
-		public float? Height { get; set; }
-		public string? Height_Unit { get; set; }
-		public float? Weight { get; set; }
-		public string? Weight_Unit { get; set; }
-		public float? Activity { get; set; }
-		public float? TDEE { get; set; }
-		public int? DailyCalories { get; set; }
-		public bool PostMethod { get; set; } = false;
-	}
+﻿namespace CaloriesManagementWeb.ViewModel
+{
+    public class CalculatorViewModel
+    {
+        public float? Age { get; set; }
+        public string? Gender { get; set; }
+        public float? Height { get; set; }
+        public string? Height_Unit { get; set; }
+        public float? Weight { get; set; }
+        public string? Weight_Unit { get; set; }
+        public float? Activity { get; set; }
+        public float? TDEE { get; set; }
+        public int? DailyCalories { get; set; }
+        public bool PostMethod { get; set; } = false;
+    }
 }
